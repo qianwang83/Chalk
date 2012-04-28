bfea20b264d0f8070a283a15a32f030f4beb4247
Qian Wang <cyberballcode@gmail.com>
2012-04-28 14:32:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chalk::Application.config.secret_token = '4f7ba9a7dc423f9bf8f705ae0969952a4e52f70448de3786f5fba067b48a579fd92818ddf69865a97798f9074eee02429f8c3134cfd9824c918f4fc06151b4c3'
